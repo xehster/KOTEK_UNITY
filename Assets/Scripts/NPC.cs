8ec94ab0ba2f13306ad0850073210d21c4873471
evk <99144157+xehster@users.noreply.github.com>
2022-08-22 11:26:55 +0400

    dialogue system updated
    added interaction icon for NPCs

---
+++ Assets/Scripts/NPC.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using DialogueEditor;
+
+public class NPC : MonoBehaviour
+{
+
+    public bool playerInRange;
+    public NPCConversation Conversation;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && playerInRange)
+        {
+            ConversationManager.Instance.StartConversation(Conversation);
+        }
+    }
+    
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInRange = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInRange = false;
+        }
+    }
+}
+    
